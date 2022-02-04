0197f98fbefd12d34b26a43d1c29124488836a22
VvsJaemin <djd453216@nate.com>
2022-02-04 23:54:32 +0900

    feat : servlet과 jsp(2)

--- ch2/src/main/java/com/jaemin/ch2/HelloServlet.java
+++ ch2/src/main/java/com/jaemin/ch2/HelloServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/hello")
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
 
 	// 서블릿이 초기화 될 때 자동 호출되는 메서드
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		System.out.println("HelloServlet init() is Called");
 	}
-	
+
 	@Override
 	protected void service(HttpServletRequest arg0, HttpServletResponse arg1) throws ServletException, IOException {
 		// TODO Auto-generated method stub
@@ -31,10 +31,8 @@ public class HelloServlet extends HttpServlet {
 
 	@Override
 	public void destroy() {
-		// 3. 뒷정리  - 서블릿이 메모리에서 제거될때 서블릿 컨테이너에 의해서 자동 호출
-
+		// 3. 뒷정리 - 서블릿이 메모리에서 제거될때 서블릿 컨테이너에 의해서 자동 호출sss
 		System.out.println("HelloServlet destroy() is Called");
 	}
 
-	
 }
