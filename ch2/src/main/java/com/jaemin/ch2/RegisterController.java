746e2c2499f617f6a6f0cafd227832b98320dc26
VvsJaemin <djd453216@nate.com>
2022-02-09 23:10:07 +0900

    feat : @GetMapping&PostMapping

--- ch2/src/main/java/com/jaemin/ch2/RegisterController.java
+++ ch2/src/main/java/com/jaemin/ch2/RegisterController.java
@@ -1,19 +1,41 @@
 package com.jaemin.ch2;
 
+import java.net.URLEncoder;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
 	
-	@RequestMapping("/register/add")
-	public String register() {
-		return "registerForm";
-	}
+//	@RequestMapping("/register/add")
+//	public String register() {
+//		return "registerForm";
+//	}
 	
-	@RequestMapping("/register/save")
-	public String save() {
+//	@RequestMapping(value = "/register/save", method=RequestMethod.POST)
+	@PostMapping("/register/save")
+	public String save(User user, Model model) throws Exception {
+		//1. 유효성 검사
+		if(!isValid(user)) {
+		
+			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+//			model.addAttribute("msg", msg);
+//			return "redirect:/register/add";
+			return "redirect:/register/add?msg="+msg; // URL 재작성(rewriting)
+		}
+		//2. DB에 신규회원 정보를 저장
+		
+
 		return "registerInfo";
+	}
+
+	private boolean isValid(User user) {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 }
