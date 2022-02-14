29935365ed1a86ec0e667958e86dbecab815d72f
VvsJaemin <djd453216@nate.com>
2022-02-14 21:51:22 +0900

    feat : Session 예제

--- ch2/src/main/java/com/jaemin/ch2/LoginController.java
+++ ch2/src/main/java/com/jaemin/ch2/LoginController.java
@@ -4,9 +4,12 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +23,20 @@ public class LoginController {
 		return "loginForm";
 	}
 
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// 1. 세션 종료
+		session.invalidate();
+		// 2. 홈으로 이동
+		return "redirect:/";
+	}
+
 	@PostMapping("/login")
-	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws UnsupportedEncodingException {
-		System.out.println("id="+id);
-		System.out.println("pwd="+pwd);
-		System.out.println("rememberId="+rememberId);
+	public String login(String id, String pwd, boolean rememberId, String toURL, HttpServletRequest request,
+			HttpServletResponse response) throws UnsupportedEncodingException {
+		System.out.println("id=" + id);
+		System.out.println("pwd=" + pwd);
+		System.out.println("rememberId=" + rememberId);
 		// 1. id와 pwd를 확인
 		if (!loginCheck(id, pwd)) {
 			// 2-1. 일치하지 않으면, loginForm으로 이동
@@ -33,19 +45,28 @@ public class LoginController {
 			return "redirect:/login/login?msg=" + msg;
 		}
 		// 2-2. id와 pwd가 일치하면, 홈으로 이동
-		if(rememberId){
+
+		// 세션 객체 얻어오기
+		HttpSession session = request.getSession();
+		// 세션 객체에 id를 저장
+		session.setAttribute("id", id);
+
+		if (rememberId) {
 			// 1. 쿠키를 생성
 			Cookie cookie = new Cookie("id", id);
 			// 2. 응답에 저장
 			response.addCookie(cookie);
-		}else {
+		} else {
 			Cookie cookie = new Cookie("id", id);
 			cookie.setMaxAge(0); // 쿠키 삭제
 			response.addCookie(cookie);
 		}
 
 		// 3. 홈으로 이동
-		return "redirect:/";
+
+		toURL = toURL == null || toURL.equals("") ? "/" : toURL;
+
+		return "redirect:" + toURL;
 	}
 
 	private boolean loginCheck(String id, String pwd) {
