2631885797ef233dc15c3c95eb180415619bce74
VvsJaemin <djd453216@nate.com>
2022-02-17 22:41:32 +0900

    feat : 데이터의 변환과 검증(2)

--- ch2/src/main/java/com/jaemin/ch2/RegisterController.java
+++ ch2/src/main/java/com/jaemin/ch2/RegisterController.java
@@ -1,14 +1,15 @@
 package com.jaemin.ch2;
 
-import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.List;
 
-import org.springframework.beans.propertyeditors.CustomDateEditor;
+import javax.validation.Valid;
+
 import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,11 +21,16 @@ public class RegisterController {
 
 	@InitBinder
 	public void toDate(WebDataBinder binder) {
+		ConversionService conversionService = binder.getConversionService();
 		/*
 		 * SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 		 * binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
 		 */
-		binder.registerCustomEditor(String[].class, new StringArrayPropertyEditor("#"));
+		binder.registerCustomEditor(String[].class, "hobby", new StringArrayPropertyEditor("#"));
+//		binder.setValidator(new UserValidator());
+		binder.addValidators(new UserValidator());
+		List<Validator> validatorList = binder.getValidators();
+		System.out.println("validatorList  = " + validatorList);
 	}
 
 	@RequestMapping(value = "/register/add", method = { RequestMethod.GET, RequestMethod.POST })
@@ -34,18 +40,25 @@ public class RegisterController {
 
 //	@RequestMapping(value = "/register/save", method=RequestMethod.POST)
 	@PostMapping("/register/save")
-	public String save(User user, BindingResult result, Model model) throws Exception {
+	public String save(@Valid User user, BindingResult result, Model model) throws Exception {
 
 		System.out.println("result = " + result);
 		System.out.println("user = " + user);
-		// 1. 유효성 검사
-		if (!isValid(user)) {
-
-			String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
-			model.addAttribute("msg", msg);
-			return "forward:/register/add";
-//			return "redirect:/register/add?msg="+msg; // URL 재작성(rewriting)
+		// 수동검증 - Validator 직접 작성 및 호출
+		/*
+		 * UserValidator userValidator = new UserValidator();
+		 * userValidator.validate(user, result);
+		 */
+		if (result.hasErrors()) {
+			return "registerForm";
 		}
+		/*
+		 * // 1. 유효성 검사 if (!isValid(user)) {
+		 * 
+		 * String msg = URLEncoder.encode("id를 잘못입력하셨습니다.", "utf-8");
+		 * model.addAttribute("msg", msg); return "forward:/register/add"; // return
+		 * "redirect:/register/add?msg="+msg; // URL 재작성(rewriting) }
+		 */
 		// 2. DB에 신규회원 정보를 저장
 
 		return "registerInfo";
